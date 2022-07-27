089d4ccdac7ba25cce5b79937bd5b45447c4aa81
Ryan Smith <ryan.chris.smith@gmail.com>
2022-07-27 09:43:14 -0600

    pass test for admit patrons to museum class'

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,10 +1,12 @@
 class Museum
   attr_reader :name,
-              :exhibits
+              :exhibits,
+              :patrons
 
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
@@ -23,5 +25,8 @@ class Museum
     recommendations
   end
 
+  def admit(patron)
+    @patrons << patron
+  end
 
 end
