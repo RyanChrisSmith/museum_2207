0cad68fc75e2e1c1ef8d3b7ed21ec8a5adce6424
Ryan Smith <ryan.chris.smith@gmail.com>
2022-07-27 09:04:19 -0600

    passing attributes test

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -7,4 +7,10 @@ RSpec.describe Patron do
     patron_1 = Patron.new("Bob", 20)
     expect(patron_1).to be_instance_of(Patron)
   end
+
+  it 'has attributes' do
+    patron_1 = Patron.new("Bob", 20)
+    expect(patron_1.name).to eq("Bob")
+    expect(patron_1.spending_money).to eq(20)
+  end
 end
