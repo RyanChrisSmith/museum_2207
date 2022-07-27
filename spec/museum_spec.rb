d64fcf8488a7109f353065657ea1ea810e0f2d3f
Ryan Smith <ryan.chris.smith@gmail.com>
2022-07-27 09:22:51 -0600

    add test for add_exhibit method

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -2,9 +2,9 @@ require './lib/exhibit'
 require './lib/patron'
 require './lib/museum'
 
-RSpec.describe Museum do 
+RSpec.describe Museum do
 
-  it 'exists' do 
+  it 'exists' do
     dmns = Museum.new("Denver Museum of Nature and Science")
     expect(dmns).to be_instance_of(Museum)
   end
@@ -17,6 +17,19 @@ RSpec.describe Museum do
   it 'has no exhibits as default in beginngin' do
     dmns = Museum.new("Denver Museum of Nature and Science")
     expect(dmns.exhibits).to eq([])
+  end
+
+  it 'can add exhibits' do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls])
+
+    dmns.add_exhibit(imax)
+    expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
   end
 
 end
