9f4f964936c9ac0839709d698f47fa34a8414889
Ryan Smith <ryan.chris.smith@gmail.com>
2022-07-27 08:57:07 -0600

    test created for exhibit class

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,11 @@
+require './lib/exhibit'
+
+RSpec.describe Exhibit do
+
+  it 'exists' do 
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    expect(exhibit).to be_instance_of(Exhibit)
+  end
+
+
+end
