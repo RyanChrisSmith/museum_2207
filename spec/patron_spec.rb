deaa27e0aa2e507fb94cb8103dce8a8066c15e92
Ryan Smith <ryan.chris.smith@gmail.com>
2022-07-27 09:06:19 -0600

    add interests empty as default test

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -1,9 +1,9 @@
 require './lib/exhibit'
 require './lib/patron'
 
-RSpec.describe Patron do 
+RSpec.describe Patron do
 
-  it 'exists' do 
+  it 'exists' do
     patron_1 = Patron.new("Bob", 20)
     expect(patron_1).to be_instance_of(Patron)
   end
@@ -12,5 +12,10 @@ RSpec.describe Patron do
     patron_1 = Patron.new("Bob", 20)
     expect(patron_1.name).to eq("Bob")
     expect(patron_1.spending_money).to eq(20)
+  end
+
+  it 'has no interests at default' do
+    patron_1 = Patron.new("Bob", 20)
+    expect(patron_1.interests).to be([])
   end
 end
